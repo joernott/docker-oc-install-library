32962703f6271d16b80ffa5df04a4b52556f14f5
Joern Ott <joern.ott@ott-consult.de>
2017-11-07 15:45:30 +0000

    Fix NodeJS URL

--- install_functions.sh
+++ install_functions.sh
@@ -69,7 +69,7 @@ gpgkey=https://dl.iuscommunity.org/pub/ius/IUS-COMMUNITY-GPG-KEY
 EOF
                 ;;
             nodejs)
-                yum -y install https://rpm.nodesource.com/pub/8.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
+                yum -y install https://rpm.nodesource.com/pub_8.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
                 ;;
         esac
     done
