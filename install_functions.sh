24f0b5de7c94889e523925f0b4646184ee154326
Joern Ott <joern.ott@ott-consult.de>
2019-07-29 17:50:19 +0000

    Update elasticsearch 7 repo

--- install_functions.sh
+++ install_functions.sh
@@ -102,7 +102,7 @@ EOF
                 cat >/etc/yum.repos.d/elasticsearch7.repo <<EOF
 [elasticsearch-7.x]
 name=Elasticsearch repository for 7.x packages
-baseurl=https://artifacts.elastic.co/packages/7.x-prerelease/yum
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
